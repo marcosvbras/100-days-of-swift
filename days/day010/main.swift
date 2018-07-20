b088dc6469d31fe6d7c10b8b07da7339680895bc
Marcos Vinícius <marcosvbras@gmail.com>
2018-07-20 20:28:35 -0300

    Some improvements in Day 010

--- days/day010/main.swift
+++ days/day010/main.swift
@@ -21,3 +21,19 @@ func returnOddNumbers() -> (num1: Int, num2: Int) {
   return (1, 3)
 }
 print(returnOddNumbers()) // (num1: 1, num2: 3)
+
+// Function labels
+func showInfo(name: String, nickname alternativeName: String) {
+  print("Name: \(name) - Alternative Name: \(alternativeName)")
+}
+showInfo(name: "Peter Quill", nickname: "Star Lord") // Name: Peter Quill - Alternative Name: Star Lord
+
+// Function with default parameters
+func doSomeStuff(word: String, upper: Bool = true) {
+  if upper {
+    print(word.uppercased())
+  } else {
+    print(word)
+  }
+}
+doSomeStuff(word: "kiwii") // KIWII
